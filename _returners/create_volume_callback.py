ed9f086c812df6ac09d314cee7e8559750abd755
menkeyi <2826634711@qq.com>
2016-11-07 17:32:33 +0800

    ceshi

--- _returners/create_volume_callback.py
+++ _returners/create_volume_callback.py
@@ -6,13 +6,14 @@ import salt.utils
 
 def returner(ret):
        jid=ret['jid']
+       id_=ret['fun_args'][0]['id']
        post_url=ret['fun_args'][0]['post_url']
        result=ret['fun_args'][0]['result']
-       result={'jid':jid,'data':result}
+       result={'jid':jid,'data':result,'id':id_}
        data=json.dumps(result)
        headers ={"Content-type":"application/json","Accept": "application/json"}
        req = urllib2.Request(post_url,data,headers)
        respones = urllib2.urlopen(req)
-       with salt.utils.fopen('/tmp/menkeyi_callback.log', 'a+') as fp_:            
-                          fp_.write('jid:\n%s\npost_url:\n%s\nresult:\n%s\n'%(jid,post_url,result))
+       with salt.utils.fopen('/tmp/tmp/create_volumes_callback.log', 'a+') as fp_:
+                          fp_.write('jid:\n%s\npost_url:\n%s\nresult:\n%s\nid:%s\n'%(jid,post_url,result,id_))
        return ret
